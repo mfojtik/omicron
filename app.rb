f582cd2d67f79628e3fa808111a200c0083c219f
Michal Fojtik <mfojtik@redhat.com>
2013-01-21 20:41:42 +0100

    Fixed design

--- app.rb
+++ app.rb
@@ -1,53 +1,59 @@
 require 'ostruct'
-require 'digest/sha1'
 require 'time'
-require 'nokogiri'
-
-require 'bundler/setup'
-Bundler.require(:default)
 
 module Mifo
 
   STORAGE = File.join(File.dirname(__FILE__), 'posts')
 
-  class Post
-    attr_reader :title, :body, :last_updated, :permalink
+  module BlogHelper
+    def markdown(text)
+      Kramdown::Document.new(text, :auto_ids => false).to_html
+    end
+    def text(t)
+      Nokogiri::HTML(t).text
+    end
+  end
 
-    def self.latest(count=10)
+  module PostClassMethods
+    def latest(count=10)
       all.reverse[0..(count-1)]
     end
 
-    def self.all
+    def all
       Dir.glob(File.join(STORAGE, '*.md')).collect do |f|
         self.new(File.basename(f).gsub(/\.md$/,''))
       end.to_a.sort { |a,b| a.last_updated <=> b.last_updated }
     end
 
-    def self.by_permalink(permalink)
-      self.new(permalink)
+    def where(opts={})
+      self.new(opts[:permalink]) if opts[:permalink]
     end
+  end
+
+  class Post
+    extend PostClassMethods
+
+    attr_reader :title, :body, :last_updated, :permalink
 
     def initialize(permalink)
       @permalink = permalink.to_s.strip
-      @raw_content = File.read(File.join(Mifo::STORAGE, @permalink + '.md'))
-      meta_data do |m|
+      @raw_content = File.read(
+        File.join(Mifo::STORAGE, @permalink + '.md')
+      ).each_line.to_a
+      metadata do |m|
         @title = m.title
         @last_updated = DateTime.parse(m.last_updated || m.updated)
       end
     end
 
     def body
-      @raw_content.each_line.to_a[metadata_end_index+1..-1].join
-    end
-
-    def sha1
-      Digest::SHA1.hexdigest(title+body)
+      @body ||= @raw_content[end_index+1..-1].join
     end
 
     private
 
-    def meta_data
-      data = @raw_content.each_line.to_a[0..metadata_end_index].inject({}) do |result, line|
+    def metadata
+      data = @raw_content[0..end_index].inject({}) do |result, line|
         property, value = line.split(':')
         result[property.intern] = value
         result
@@ -55,8 +61,8 @@ module Mifo
       yield OpenStruct.new(data) if block_given?
     end
 
-    def metadata_end_index
-      @raw_content.each_line.to_a.index { |l| l =~ /^(\#+)$/ } || 0
+    def end_index
+      @raw_content.index { |l| l =~ /^(\#+)$/ } || 0
     end
 
   end
@@ -67,38 +73,22 @@ module Mifo
       status 404
     end
 
-    helpers do
-      def markdown(text)
-        Kramdown::Document.new(text, :auto_ids => false).to_html
-      end
-      def text(t)
-        Nokogiri::HTML(t).text
-      end
-    end
+    helpers BlogHelper
 
     get '/' do
       @posts = Post.latest
-      #etag sha1(@posts.map { |p| p.sha1 }.join )
       haml :index
     end
 
     get %r{/(rss|atom|articles)(.xml)?} do
-      @posts = Post.latest
-      etag sha1(@posts.map { |p| p.sha1 }.join )
       content_type 'text/xml;charset=utf-8'
+      @posts = Post.latest
       haml(:rss, :format => :xhtml, :escape_html => true, :layout => false)
     end
 
     get '/:permalink' do
-      @post = Post.by_permalink(params[:permalink])
-      # etag @post.sha1
+      @post = Post.where(:permalink => params[:permalink])
       haml :show, :ugly => true
-    end
-
-    private
-
-    def sha1(obj)
-      Digest::SHA1.hexdigest(obj)
     end
 
   end
